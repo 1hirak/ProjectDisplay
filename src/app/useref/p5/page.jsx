90dde4b5080b6c86f9fe7f977477f39ff0c0cc01
Hirak Das - github.com/1hirak <jhirak83@gmail.com>
2025-03-14 01:25:40 +0530

    update

--- src/app/useref/p5/page.jsx
+++ src/app/useref/p5/page.jsx
@@ -1,34 +1,38 @@
-'use client'
-import { useRef, useState } from 'react'
-
+"use client";
+import { useRef, useState, useEffect } from "react";
 function DynamicTextarea() {
-  const [value, setValue] = useState('')
-  const textareaRef = useRef(null)
-
+  const [value, setValue] = useState("");
+  const textareaRef = useRef(null);
+  const widthRef = useRef(null);
+  useEffect(() => {
+    widthRef.current = window.innerWidth;
+    console.log(widthRef.current);
+  }, []);
   const handleInput = (e) => {
-    setValue(e.target.value)
-    
-    /* Implement:  
-    1. Measure the textarea's scrollHeight  
-    2. Update the textarea's height to match its content  
-    */
-  }
-
-  console.log(textareaRef.current);
-  console.log(Window.scrollHeight)
-  
-
+    setValue(e.target.value);
+  };
+  const handleTextareaFocus = () => {
+    textareaRef.current.style.height = "400px";
+    textareaRef.current.style.width = `${widthRef.current}px`;
+  };
+  const handleTextareaBlur = () => {
+    textareaRef.current.style.height = "48px";
+    textareaRef.current.style.width = "50%";
+  };
   return (
-    <div>
+    <div className="flex justify-center ml-10 mr-10 mt-5">
       <textarea
+        className="p-1.5 rounded-md"
         ref={textareaRef}
         value={value}
         onInput={handleInput}
-        style={{ overflow: 'hidden', resize: 'none' }}
+        onFocus={handleTextareaFocus}
+        onBlur={handleTextareaBlur}
+        style={{ overflow: "hidden", resize: "none", border: "1px,solid,red" }}
         placeholder="Type something..."
       />
     </div>
-  )
-}
 
-export default DynamicTextarea
+  );
+}
+export default DynamicTextarea;
