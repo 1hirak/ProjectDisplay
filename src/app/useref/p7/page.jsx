123feff736441ccb6f71a22087e60fc6be9777c6
Hirak Das - github.com/1hirak <jhirak83@gmail.com>
2025-03-15 04:03:13 +0530

    mvp completion  of drawing pad

--- src/app/useref/p7/page.jsx
+++ src/app/useref/p7/page.jsx
@@ -1,43 +1,44 @@
-'use client'
-import { useRef, useState, useEffect } from 'react'
+"use client";
+import { useState,useRef, useEffect } from "react";
 
 export default function DrawingBoard() {
-  const canvasRef = useRef(null)
-  const isDrawing = useRef(false)
-  const [brushColor, setBrushColor] = useState('#000000')
+  const canvasRef = useRef(null);
+  const isDrawing = useRef(false);
+  const [brushColor, setBrushColor] = useState("#000000");
+  const ctxRef = useRef(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current
-    if (!canvas) return
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const ctx = canvasRef.current.getContext("2d");
+    ctxRef.current = ctx;
+    ctx.beginPath();
+    ctx.strokeStyle = brushColor;
+    ctx.lineWidth = 3;
+  }, [brushColor]);
 
-    /* Implement:  
-    1. Set up canvas context  
-    2. Set default stroke style  
-    */
-  }, [])
+  const getCoordinates = (e) => {
+    let val = canvasRef.current.getBoundingClientRect();
+    return [e.clientX - val.x, e.clientY - val.y];
+  };
 
   const startDrawing = (e) => {
-
-    console.log(e)
-    /* Implement:  
-    1. Get canvas coordinates  
-    2. Begin path  
-    3. Update isDrawing ref  
-    */
-  }
+    isDrawing.current = true;
+    const [x, y] = getCoordinates(e);
+    ctxRef.current.moveTo(x, y);
+  };
 
   const draw = (e) => {
-    if (!isDrawing.current) return
+    if (!isDrawing.current) return;
 
-    console.log(e)
-    /* Implement:  
-    1. Update path coordinates  
-    2. Draw line  
-    */
-  }
+    const [p, q] = getCoordinates(e);
+    
+    ctxRef.current.lineTo(p, q);
+    ctxRef.current.stroke();
+  };
 
   return (
-    <div className='flex flex-col items-center w-full gap-2 mt-4'>
+    <div className="flex flex-col items-center w-full gap-2 mt-4">
       <canvas
         ref={canvasRef}
         width={800}
@@ -46,13 +47,13 @@ export default function DrawingBoard() {
         onMouseMove={draw}
         onMouseUp={() => (isDrawing.current = false)}
         onMouseLeave={() => (isDrawing.current = false)}
-        style={{ border: '1px solid' }}
+        style={{ border: "1px solid" }}
       />
-      <input 
-        type="color" 
-        value={brushColor} 
-        onChange={(e) => setBrushColor(e.target.value)} 
+      <input
+        type="color"
+        value={brushColor}
+        onChange={(e) => setBrushColor(e.target.value)}
       />
     </div>
-  )
+  );
 }
