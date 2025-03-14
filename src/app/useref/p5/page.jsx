9345fae824f3a5960434f9ee84c3e085ab34b840
Hirak Das - github.com/1hirak <jhirak83@gmail.com>
2025-03-15 04:36:20 +0530

    update

--- src/app/useref/p5/page.jsx
+++ src/app/useref/p5/page.jsx
@@ -2,7 +2,7 @@
 import { useRef, useState, useEffect } from "react";
 function DynamicTextarea() {
   const [value, setValue] = useState("");
-  const [focusArea, setfocusArea] = useState("default")
+  const [focusArea, setfocusArea] = useState("default");
   const textareaRef = useRef(null);
   const widthRef = useRef(null);
   useEffect(() => {
@@ -13,33 +13,40 @@ function DynamicTextarea() {
     setValue(e.target.value);
   };
   const handleTextareaFocus = () => {
-    textareaRef.current.style.border= "3px solid red";
+    textareaRef.current.style.border = "3px solid red";
     textareaRef.current.style.height = "400px";
-    textareaRef.current.style.width = `${widthRef.current*.9}px`;
+    textareaRef.current.style.width = `${widthRef.current * 0.9}px`;
     textareaRef.current.style.padding = "20px";
-    setfocusArea(`focusing`)
+    setfocusArea(`focusing`);
   };
   const handleTextareaBlur = () => {
     textareaRef.current.style.height = "48px";
     textareaRef.current.style.width = "50%";
     textareaRef.current.style.padding = "6px";
-    textareaRef.current.style.border= "1px solid grey";
-    setfocusArea("focus removed")
+    textareaRef.current.style.border = "1px solid grey";
+    setfocusArea("focus removed");
   };
-  return ( <div className="ml-10 mr-10 mt-5" >
-    <div className="flex flex-col items-center gap-4" >
-      <div>{focusArea}</div>
-      <textarea
-        className="p-1.5 rounded-md"
-        ref={textareaRef}
-        value={value}
-        onInput={handleInput}
-        onFocus={handleTextareaFocus}
-        onBlur={handleTextareaBlur}
-        style={{ overflow: "hidden", resize: "none", border: "1px solid grey",width:"50%" }}
-        placeholder="Type something..."
-      />
-    </div></div>
+  return (
+    <div className="ml-10 mr-10 mt-5">
+      <div className="flex flex-col items-center gap-4">
+        <div>{focusArea}</div>
+        <textarea
+          className="p-1.5 rounded-md"
+          ref={textareaRef}
+          value={value}
+          onInput={handleInput}
+          onFocus={handleTextareaFocus}
+          onBlur={handleTextareaBlur}
+          style={{
+            overflow: "hidden",
+            resize: "none",
+            border: "1px solid grey",
+            width: "50%",
+          }}
+          placeholder="Type something..."
+        />
+      </div>
+    </div>
   );
 }
 export default DynamicTextarea;
