4b7d542bf56baff16c9ad78eef96166e0ba173f9
Hirak Das - github.com/1hirak <jhirak83@gmail.com>
2025-03-20 22:55:47 +0530

    working on blog post :  pagination added

--- src/app/useref/p9/cardList/page.jsx
+++ src/app/useref/p9/cardList/page.jsx
@@ -1,17 +1,71 @@
+'use client'
 import React from "react";
 import ImprovedBlogCard from "../design3/ImprovedBlogCard";
 import { data } from "@/data/blogpost";
+import { useState,useEffect } from "react";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+
+
 
 const cardList = () => {
+
+  const [first, setfirst] = useState([]);
+
+  useEffect(() => {
+    setfirst(data.slice(0,24))
+  
+    return () => {
+      
+    }
+  }, [])
+  
   return (
     <div
     className="flex flex-wrap gap-4 justify-center">
 
+
+
+
+
       {/* <img src="/media/images/10.jpg" alt=""className="w-20 h-20" /> //to test the location*/}
-      {data.map((x) => (
+      {first.map((x) => (
         <ImprovedBlogCard key={x.id} post={x} />
       ))}
-
+  <div
+  className="mt-6 mb-6">
+  <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" isActive>
+            2
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+    </div>
     </div>
   );
 };
